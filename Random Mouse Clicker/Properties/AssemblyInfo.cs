88d7cabc8a4c9f9afb4ea2743d5332df82d254b9
Milan <hi.milan102@gmail.com>
2017-10-22 02:37:38 -0700

    Updated file to version 1.2

--- Random Mouse Clicker/Properties/AssemblyInfo.cs
+++ Random Mouse Clicker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Random Mouse Clicker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Auto clicks within a specified region")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Random Mouse Clicker")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
